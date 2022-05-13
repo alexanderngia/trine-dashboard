5acc3f7ac6bed5598515262e89899259a4dbdaf8
Alexander <alexanderngia@gmail.com>
2022-05-13 23:55:36 +0700

    productList

--- src/components/views/productList/productList.tsx
+++ src/components/views/productList/productList.tsx
@@ -1,20 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./productList.module.scss";
-// import { useHistory } from "react-router-dom";
 import { Layout } from "../layout/layout";
+import { ButtonMain } from "../../ui/button/button";
+import CardList from "../../ui/card/cardList/cardList";
+import { IoAdd, IoDownloadOutline } from "react-icons/io5";
 export interface ProductListProps {}
 
 const ProductList: React.FC<ProductListProps> = (props) => {
-  // const history = useHistory();
+  const [idProduct, setIdProduct] = useState(`ALL`);
+  const [data, setData] = useState([]);
 
-  // const logout = () => {
-  //   localStorage.removeItem("accessToken");
-  //   history.replace("/login");
-  // };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/api/warehouse?id=${idProduct}`
+        );
+        const resData = res.data.products;
 
+        setData(resData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [idProduct]);
   return (
     <Layout>
-      <div className={styles["root"]}>ProductList</div>
+      <div className={styles["root"]}>
+        <h1>DANH SÁCH SẢN PHẨM</h1>
+        <div className={styles["btn-container"]}>
+          <div className={styles["filter"]}>
+            <ButtonMain>LINGERIA</ButtonMain>
+            <ButtonMain>ACCESSORIES</ButtonMain>
+            <ButtonMain>DRESS</ButtonMain>
+          </div>
+          <div className={styles["btnCrud"]}>
+            <ButtonMain>
+              <IoAdd className={styles["icon"]} />
+            </ButtonMain>
+            <ButtonMain>
+              <IoDownloadOutline className={styles["icon"]} />
+            </ButtonMain>
+          </div>
+        </div>
+        {data.length > 0 && (
+          <ul className={styles["card-container"]}>
+            {React.Children.toArray(
+              data.map((listItems: any) => {
+                return (
+                  <CardList className={styles["card-custom"]}>
+                    <a href="/#">
+                      <ul>
+                        <li>
+                          <img
+                            src={listItems.imgItem}
+                            alt={listItems.imgItem}
+                          />
+                        </li>
+                        <li>{listItems.nameItem}</li>
+                        <li>{listItems.qualityItem}</li>
+                        <li>{listItems.colorItem}</li>
+                        <li>{listItems.sizeItem}</li>
+                        <li>{listItems.priceItem}</li>
+                      </ul>
+                    </a>
+                  </CardList>
+                );
+              })
+            )}
+          </ul>
+        )}
+      </div>
     </Layout>
   );
 };
